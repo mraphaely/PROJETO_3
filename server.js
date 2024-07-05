b878b2db61e7797fefc9fa04f42472a8addf31ae
mraphaely <raphaelymaryana@gmai.com>
2024-07-05 13:59:26 -0300

    Rota GET/receitas finalizada

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 import { createServer } from 'node:http';
 import fs from "node:fs";
 
+import lerDadosReceita from './helper/lerReceitas.js';
+
 const PORT = 3333;
 
 const server = createServer((request, response)=>{
     const { url, method } = request;
-
+    //#01 - adicionar a rota GET receitas
     if(method === "GET" && url === "/receitas"){
-
+      lerDadosReceita((err, receitas)=>{
+        if(err){
+          response.writeHead(500, {"Content-Type":"apllication/json"});
+          response.end(JSON.stringify({message: "Erro ao ler dados"}));
+          return
+        }
+        //imprimir resultado
+          response.writeHead(200, {"Content-Type":"apllication/json"});
+          response.end(JSON.stringify(receitas));
+      })      
     }else if(method === "POST" && url === "/receitas"){
 
     }else if(method === "GET" && url.startsWith("/receitas/")){
