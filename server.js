6967a2a05e3c06842a036424fab24de0e9bc2d30
mraphaely <raphaelymaryana@gmai.com>
2024-07-08 14:19:47 -0300

    Rota cadastro de receitas e ADD UUID

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import { createServer } from 'node:http';
 import fs from "node:fs";
+import { v4 as uuidv4 } from 'uuid';
 
 import lerDadosReceita from './helper/lerReceitas.js';
 const receitas = [];
 const PORT = 3333;
+
+//link: https://www.notion.so/Atividade-03-7338c4483a174f499a17ba6dc82d1e3c
 
 const server = createServer((request, response)=>{
     const { url, method } = request;
@@ -11,31 +14,47 @@ const server = createServer((request, response)=>{
     if(method === "GET" && url === "/receitas"){
       lerDadosReceita((err, receitas)=>{
         if(err){
-          response.writeHead(500, {"Content-Type":"apllication/json"});
+          response.writeHead(500, {"Content-Type":"application/json"});
           response.end(JSON.stringify({message: "Erro ao ler dados"}));
           return
         }
         //imprimir resultado
-          response.writeHead(200, {"Content-Type":"apllication/json"});
+          response.writeHead(200, {"Content-Type":"application/json"});
           response.end(JSON.stringify(receitas));
       })      
     }else if(method === "POST" && url === "/receitas"){
         //nome, categoria, ingredientes e modo de preparo.
-           let body = ''
+           let body = '';
            request.on('data', (chunk)=>{
-            body += chunk.toString()
+            body += chunk;
            });
            request.on('end', ()=>{
-            const receita = JSON.parse(body)
-            if (body.nome === '' || body.categoria === '' || body.ingredientes === '' || body.modoDePreparo === '') {
-                response.writeHead(404, { 'Content-Type': 'application/json' })
-                response.end(JSON.stringify({ message: 'Preencha corretamente' }));
+            const novaReceita = JSON.parse(body)
+            lerDadosReceita((err, receitas)=>{
+              if(err){
+              response.writeHead(500, {"Content-Type":"application/json"});
+              response.end(JSON.stringify({message: "Erro ao ler dados"}));
+              return
+              }
+            })
+            // if (body.nome === '' || body.categoria === '' || body.ingredientes === '' || body.modoDePreparo === '') {
+            //     response.writeHead(404, { 'Content-Type': 'application/json' })
+            //     response.end(JSON.stringify({ message: 'Preencha corretamente' }));
+            //     return
+            // }
+            // novaReceita.id = receitas.length + 1
+            novaReceita.id = uuidv4();
+            receitas.push(novaReceita)
+            fs.writeFile("receitas.json", JSON.stringify(receitas, null, 2), (err) => {
+              if(err){
+                response.writeHead(500, {"Content-Type":"application/json"});
+                response.end(JSON.stringify({message: "Erro ao ler receita"}));
                 return
-            }
-            receita.id = receitas.length + 1
-            receitas.push(receita)
-            response.writeHead(201, { "Content-Type": "application/json" })
-            response.end(JSON.stringify(receitas));
+                }
+                response.writeHead(201, { "Content-Type": "application/json" })
+                response.end(JSON.stringify({novaReceita}));
+            })  
+
         })
     }else if(method === "GET" && url.startsWith("/receitas/")){
 
